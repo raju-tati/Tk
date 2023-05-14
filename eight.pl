75de81ccc2199c2b3ac1a975934286a4dfdfc3d8
raju-tati <118584250+raju-tati@users.noreply.github.com>
2023-05-14 20:27:40 +0530

    TextBox

--- eight.pl
+++ eight.pl
@@ -4,15 +4,28 @@ use utf8;
 
 sub main() {
     use Tk;
+    use Tk::Photo;
     
     my $mainWindow = MainWindow->new();
-    $mainWindow->title("Tk Text");
+    $mainWindow->title("Tk Window");
 
     my $textBox = $mainWindow->Text(
         -width => 120,
         -height => 50,
         -font => "newspaper 10"
     );
+
+    $textBox->insert('end', "Some text to insert");
+
+    my $photo = $textBox->Photo(-format => 'bmp',
+                                -file => 'Desert.bmp');
+    
+     my $canvas = $mainWindow->Canvas(
+        -width => $photo->width,
+        -height => $photo->height,
+    )->pack(-side => 'bottom');
+    
+    $canvas->createImage(0,0, -image => $photo, -anchor => 'nw');
 
     $textBox->pack();
     
